86313a031f8d51e16cf3bdb7b7228c51884cfaa1
hamza1220 <40993049+hamza1220@users.noreply.github.com>
2019-11-17 17:18:47 +0500

    Explore_Markers

--- components/Explore.js
+++ components/Explore.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
-import { AppRegistry, StyleSheet, Dimensions, View,  Container, Text } from "react-native";
+import { AppRegistry, StyleSheet, Dimensions, View,  Container, Text, Image } from "react-native";
 import { TabNavigator } from "react-navigation";
-import MapView from 'react-native-maps'
+import MapView, { Callout } from 'react-native-maps'
+
+class CustomCalloutView extends React.Component {
+
+    render() {
+        return (
+          this.props.navigation.navigate('HomeScreen')
+        )
+    }
+}
+
 
 export default class Explore extends React.Component {
   constructor(props) {
@@ -11,13 +21,24 @@ export default class Explore extends React.Component {
       latitude: 31.5204,
       longitude: 74.3587,
       error:null,
+
+      markers:[
+                {T_ID: 1, location: {latitude: 31.471728, longitude: 74.410013}},
+                {T_ID: 2, location: {latitude: 31.471817, longitude: 74.410567}},
+              ]
+
     };
   }
+
+  // markers:[{T_ID: 1, planter: "hamzaahmad86@gmail.com", SP_ID: 11, planted: "17/11/19", watered: "17/11/19", location: {latitude: 31.471728, longitude: 74.410013}, images: []},
+  //             {T_ID: 2, planter: "hamzaahmad86@gmail.com", SP_ID: 22, planted: "16/11/19", watered: "16/11/19", location: {latitude: 31.471817, longitude: 74.410567}, images: []},
+  //             {T_ID: 3, planter: "hamzaahmad86@gmail.com", SP_ID: 33, planted: "15/11/19", watered: "17/11/19", location: {latitude: 31.471675, longitude: 74.410688}, images: []},
+  //             {T_ID: 4, planter: "hamzaahmad86@gmail.com", SP_ID: 44, planted: "14/11/19", watered: "17/11/19", location: {latitude: 31.471881, longitude: 74.410431}, images: []},
+  //             ]
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
        (position) => {
-         console.log("wokeeey");
          console.log(position);
          this.setState({
            latitude: position.coords.latitude,
@@ -27,9 +48,24 @@ export default class Explore extends React.Component {
        },
        (error) => this.setState({ error: error.message }),
        { enableHighAccuracy: true, timeout: 200000 },
-     );
+    );
+
+    if(this.props.toShow == undefined){
+      this.getMarkers(-1);
+    }
+    else{
+      this.getMarkers(this.props.toShow);
+    }
+
    }
 
+  getMarkers = (treesToShow) => {
+      console.log(treesToShow)
+  }
+
+  onMarkerPress = (e, loc) => {
+    console.log(e, loc)
+  }
 
   render() {
     return (
@@ -44,7 +80,14 @@ export default class Explore extends React.Component {
         showsUserLocation = {true}
         showsMyLocationButton = {true}
         loadingEnabled = {true}
-      />
+      >
+        {this.state.markers.map((marker,i) => (
+          <MapView.Marker coordinate={marker.location} title={(marker.T_ID.toString())} key = {i} onPress = {(e) => {this.onMarkerPress(e,marker.location)}}>
+            <Image source={require('../tree_marker.png')} style={{width:20, height:17}} />
+            
+          </MapView.Marker>
+        ))} 
+      </MapView>
 
     );
   }
