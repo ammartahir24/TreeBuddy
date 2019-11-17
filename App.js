b2437aae76e6dcafaf6b113b2f95373e3258ec17
HamzaJawed <m.hamza@live.com>
2019-11-17 12:40:13 +0500

    navigation improved

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import HomeScreen from './components/HomeScreen'
+import Explore from './components/Explore'
+import {createDrawerNavigator} from "react-navigation-drawer";
+
+
 import {
   StyleSheet,
   Button,
@@ -15,12 +19,30 @@ const AppNavigator = createStackNavigator({
   Home: {
     screen: HomeScreen,
   },
+  Explore : {
+    screen: Explore,
+  },
 });
 
 const AppContainer = createAppContainer(AppNavigator);
 
+
+const MyDrawerNavigator = createDrawerNavigator({
+  Home: {
+    screen: AppContainer,
+  },
+  Explore : {
+    screen: Explore,
+  },
+  // Notifications: {
+  //   screen: MyNotificationsScreen,
+  // },
+});
+
+const MyApp = createAppContainer(MyDrawerNavigator);
+
 export default class App extends React.Component {
   render() {
-    return <AppContainer />;
+    return <MyApp />;
   }
 }
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -13,22 +13,11 @@ import {
 export default class HomeScreen extends React.Component {
     static navigationOptions = {
       title: 'Welcome',
-      headerLeft: <TouchableOpacity onPress={() => navigation.openDrawer()}> 
+      headerLeft: <TouchableOpacity onPress={() => this.props.navigation.openDrawer()}> 
 
       <Image source={require('../hamburger.png')} style={{width:20, height:17, marginLeft: 20}} />
-
   </TouchableOpacity>,
     };
-    // static navigationOptions = {
-    //   headerTitle: () => <LogoTitle />,
-    //   headerRight: () => (
-    //     <Button
-    //       onPress={() => alert('This is a button!')}
-    //       title="Info"
-    //       color="#fff"
-    //     />
-    //   ),
-    // };
     render() {
       const {navigate} = this.props.navigation;
       return (
