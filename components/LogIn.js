878aa3fff5939a81f1ebf89f9ed09e6b7c885198
mHassan11 <mhassan.3939@gmail.com>
2019-11-20 23:33:51 +0500

    .

--- components/LogIn.js
+++ components/LogIn.js
@@ -1,109 +1,158 @@
-import React from "react";
+import React, { Component } from "react";
 import {
     StyleSheet,
+    View,
+    Button,
+    TouchableWithoutFeedback,
+    TouchableHighlight, 
+    TextInput,
     Text,
-    Font,
     TouchableOpacity,
-    Image,
-    navigation
+    Alert,
+    Keyboard
+    // TouchableHighlight
   
 } from 'react-native';
-import {
-    Container,
-    Header,
-    Title,
-    Content,
-    Icon,
-    Body,
-    Left,
-    Right,
-    Input,
-    Item,
-    Form,
-    Label,
-    Button,
-    // Text,
-    // Font
-} from "native-base";
+
+import t from 'tcomb-form-native';
 import {signin} from "../firebase_integeration/databasefunctions"
 
-const check = async() => {
-  signin("20100108@lums.edu.pk","hamza1234")
+const check = async(userID, passWord) => {
+  // Keyboard.dismiss();
+  console.log("email enterd: ", userID)
+  console.log("password entered (dw, we aint storing your password): ", passWord)
+  // signin("20100108@lums.edu.pk","hamza1234")
+  // let isValid;
+  let isValid = await signin(userID,passWord)
+  
+  console.log("Ek min: ", isValid)
+
+  if(isValid == "false")
+  {
+    ans = "false";
+    Alert.alert(
+      'Login Failed',
+      '',
+      [
+        {text: 'Try again', onPress: () => console.log('trying again')},
+        
+        // {text: 'OK', onPress: () => console.log('OK Pressed')},
+      ],
+      {cancelable: false},
+
+  );
+      return ans;
+    
+  }
+  else if(isValid == "true")
+  {
+    // this.props.navigation.navigate("App");
+    return "true";
+  }
+
+  return;
+
 }
 
-interface State {
-  email: string;
-  password: string;
-}
 
-export default class LogIn extends React.Component
-{
+const Form = t.form.Form;
+const User = t.struct({
+  email: t.String,
+  password: t.String,
+});
+
+const options = {
+  fields: {
+    email: {
+      lable: 'Email',
+      error: 'Its only fair if we know each other'
+    },
+    password: {
+      error: 'Sorry, wont let you go without the password :)'
+    },
+  },
+};
+
+export default class LogIn extends Component {
+
+ handleSubmit = async () => {
+    const value = this._form.getValue(); // use that ref to get the form value
+    // console.log('username: ', value.username);
+    // console.log('password: ', value.password);
+    let ans = await check(value.email, value.password)
+    if(ans == 'true')
+    {
+      this.props.navigation.navigate("App");
+    }
+    else if(ans == "false")
+    {
+      Keyboard.dismiss();
+
+        this.state.email = '';
+        this.state.password =  '';
+    }
+
+
+  }
+
+
+
 
   static navigationOptions = {
       title: '  Log In', //keep log at tab space to align with the fields below,
-      drawerLockMode: 'locked-closed',
-    };
+ };
 
-
-  render() 
-  {
-    
+  render() {
     const {navigate} = this.props.navigation;
     return (
-    <Container style={styles.container}>
-        
+      <View style={styles.container}>
+        <Form 
+          ref={c => this._form = c} // assign a ref
+          type={User}
+          options={options} 
+        />
+        <Button
+          title="Login to your accounr"
+          onPress={this.handleSubmit}
+          // onPress={()=>{this.props.navigation.navigate("App")}}
+        />
 
-        <Content padder>
-          <Form>
 
-            <Item fixedLabel>
-              <Label>Username</Label>
-              <Input />
-            </Item>
+        <TouchableOpacity onPress={()=>{this.props.navigation.navigate("SignUp")}}>
+          <Text style={{textAlignVertical: "center",textAlign: "center", marginTop: 10}}> Dont have an account </Text>
+        </TouchableOpacity>
 
-            <Item fixedLabel last>
-              <Label>Password</Label>
-              <Input secureTextEntry />
-            </Item>
 
-            <Button block style={{ margin: 15, marginTop: 50}} onPress={()=>{this.props.navigation.navigate("App")}}>
-            {/* <Button block style={{ margin: 15, marginTop: 50}} onPress={()=>{check()}}> */}
-              <Text>Sign In</Text>
-            </Button>
-          
-
-          </Form>
-
-            
-          <TouchableOpacity>
-            <Text style={{textAlignVertical: "center",textAlign: "center"}}> Dont have an account </Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity >
-            <Text style={{textAlignVertical: "center",textAlign: "center"}}> Forget Password </Text>
-          </TouchableOpacity>
-
-        </Content>
-
-    </Container>
-    
+      </View>
     );
   }
 }
 
-
-const styles = StyleSheet.create
-({
-  container: 
-  {
-    backgroundColor: "#FFF"
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    marginTop: 0,
+    padding: 20,
+    backgroundColor: '#ffffff',
   },
-  text_below: {
-    // textAlign: 'center', // <-- the magic
-    fontWeight: 'bold',
-    fontSize: 24,
-    marginTop: 10,
-    width: 150,
-    // backgroundColor: 'green',
+  inputsContainer: {
+    flex: 1
+  },
+  fullWidthButton: {
+    marginTop: "10%",
+    marginRight: "5%",
+    marginLeft: "5%",
+    backgroundColor: '#017745',
+    height: "140%",
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30,
+  },
+  fullWidthButtonText: {
+    textAlignVertical: "center",
+    paddingBottom: "10%",
+    fontSize:24,
+    color: 'gold'
   }
 });
+
--- components/SignUp.js
+++ components/SignUp.js
@@ -1,98 +1,89 @@
-import React from "react";
+import React, { Component } from "react";
 import {
     StyleSheet,
+    View,
+    Button,
+    TouchableWithoutFeedback,
+    TouchableHighlight, 
+    TextInput,
     Text,
-    Font,
     TouchableOpacity,
-    Image,
-    navigation
+    AppRegistry,
+    ImageBackground
   
 } from 'react-native';
-import {
-    Container,
-    Header,
-    Title,
-    Content,
-    Icon,
-    Body,
-    Left,
-    Right,
-    Input,
-    Item,
-    Form,
-    Label,
-    Button,
-    // Text,
-    // Font
-} from "native-base";
 
-interface State {
-  email: string;
-  password: string;
-  username: string;
-  age: string
-}
+import t from 'tcomb-form-native';
+import {signin} from "../firebase_integeration/databasefunctions"
 
-export default class SignUp extends React.Component <{}, State>
-{
-
+const Form = t.form.Form;
+const User = t.struct({
+  username: t.String,
+  email: t.String,
+  password: t.String,
+  age: t.Number
+});
+const options = {
+  fields: {
+    email: {
+      error: 'Without an email address how are you going to reset your password when you forget it?'
+    },
+    username: {
+      error: 'Its only fair if we know each other\'s name. Isnt it.'
+    },
+    password: {
+      error: 'Choose something you use on a dozen other sites or something you won\'t remember'
+    },
+    age: {
+      error: 'If this takes off, we will send you a birthday card.',
+    },
+  },
+};
+export default class SignUp extends Component {
 
   static navigationOptions = {
-      title: '  New Account', //keep log at tab space to align with the fields below
-      
+      title: 'Create Account', //keep log at tab space to align with the fields below,
+      drawerLockMode: 'locked-closed',
     };
+  
+   handleSubmit = () => {
+    const value = this._form.getValue(); // use that ref to get the form value
+    console.log('value: ', value);
+  }
 
-
-  render() 
-  {
-    
-    const {navigate} = this.props.navigation;
+  render() {
     return (
-    <Container style={styles.container}>
-        
 
-        <Content padder>
-          <Form>
-
-            <Item>
-              <Label>Username</Label>
-              <Input placeholder="Muhammad Hassan"/>
-            </Item>
-
-            <Item>
-              <Label>Email</Label>
-              <Input placeholder="JBund007@gmail.com" />
-            </Item>
-            <Item>
-              <Label>Age</Label>
-              <Input placeholder="22" />
-            </Item>
-
-            <Item last>
-              <Label>Password</Label>
-              <Input secureTextEntry placeholder="****" />
-            </Item>
-
-            <Button block style={{ margin: 15, marginTop: 50}}>
-              <Text>Create Account</Text>
-            </Button>
+      <View style={styles.container}>
+        <Form 
+          ref={c => this._form = c} 
+          type={User}
+          options={options}
+        />
           
-
-          </Form>
-            
-
-        </Content>
-
-    </Container>
+        <Button
+          title="That's right Dawg, create it"
+          onPress={this.handleSubmit}
+        />
+      </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    marginTop: 0,
+    padding: 50,
+    // backgroundColor: '#ffffff',
+    // flex: 1,
+    // alignItems: 'center',
 
-const styles = StyleSheet.create
-({
-  container: 
-  {
-    backgroundColor: "#FFF"
-  }
+    //hex color below
+    // backgroundColor: '#144314',
+
+  },
+
 });
+
+AppRegistry.registerComponent('SignUp', () => SignUp);
