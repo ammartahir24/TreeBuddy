f5d531bfa9393772c9df398667e1e94345e6184e
mHassan11 <mhassan.3939@gmail.com>
2019-11-24 14:29:02 +0500

    .

--- firebase_integeration/databasefunctions.js
+++ firebase_integeration/databasefunctions.js
@@ -1,5 +1,7 @@
 import {auth, db, firebase} from './config';
 
+const image2base64 = require('image-to-base64');
+
 async function is_valid_user(email, password) {
     let isValid = true;
     await auth.signInWithEmailAndPassword(email, password).catch((err) => {
@@ -141,7 +143,7 @@ async function getAllPlants()
 {
   var start = new Date();
   let data;
-  let snapshot = await database.collection('plants').get()
+  let snapshot = await db.collection('plants').get()
     .then(snap => {
       // console.log(snap)
       if (snap.empty) {
@@ -171,13 +173,13 @@ async function getSinglePlant(plant_name)
 {
   var start = new Date();
   let data;
-  let snapshot = await database.collection('plants').where('Name', '==',plant_name).get()
+  let snapshot = await db.collection('plants').where('Name', '==',plant_name).get()
     .then(snap => {
       // console.log(snap)
       if (snap.empty) {
               console.log('query failed')
               return "QueryFailure"
-              
+
           }
           else
           {
@@ -221,7 +223,7 @@ async function getPlants(plant_name){
 async function get_AllSpecies (){
   var start = new Date();
   let data;
-  let snapshot = await database.collection('species').get()
+  let snapshot = await db.collection('species').get()
     .then(snap => {
       // console.log(snap)
       if (snap.empty) {
@@ -251,7 +253,7 @@ async function get_AllSpecies (){
 async function getSingleSpecie(specie_name){
   var start = new Date();
   let data;
-  let snapshot = await database.collection('species').where("Specie_Name",'==', specie_name).get()
+  let snapshot = await db.collection('species').where("Specie_Name",'==', specie_name).get()
     .then(snap => {
       // console.log(snap)
       if (snap.empty) {
@@ -297,6 +299,254 @@ async function getSpecieFunc (input){
 }
 
 
+////////////////////// Adding Plant Details ////////////////////////
+
+
+async function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+async function Confirm_UniqueID(table_name, ID_name,ID_value){
+
+  var start = new Date();
+  let data;
+  let snapshot = await db.collection(table_name).where(ID_name,'==', ID_value).get()
+    .then(snap => {
+      // console.log(snap)
+      if (snap.empty) {
+              // console.log('query failed')
+              // console.log("len ", Object.keys(data).length);
+              data = "empty";
+              // return "empty"
+              // return data;
+              
+          }
+          else
+          {
+              console.log("query completed in:" , new Date() - start)
+              const data_fetched = snap.docs.map(doc => doc.data());
+              data = data_fetched
+              console.log("len ", Object.keys(data).length);
+
+              return "notempty" 
+              
+          }
+
+    })
+    .catch(function(error) {
+      console.error("Error getting: ", error);
+  });
+
+    return data;
+
+}
+
+async function getSpecieNameFromSPid(Specie_ID){
+
+  var start = new Date();
+  let data;
+  let snapshot = await db.collection("species").where("Specie_ID",'==',Specie_ID).get()
+    .then(snap => {
+      // console.log(snap)
+      if (snap.empty) {
+              console.log('query failed')
+              // console.log("len ", Object.keys(data).length);
+
+              return "empty"
+              // return data;
+              
+          }
+          else
+          {
+              // console.log("query completed in:" , new Date() - start)
+              console.log(snap.id)
+              const data_fetched = snap.docs.map(doc => doc.data());
+              data = data_fetched[0]
+              console.log(data)
+              specie_name = data ['Specie_Name']
+              specie_bioname = data ['Specie_bioName']
+              // console.log("specie name ", specie_name, specie_bioname);
+
+              return {data}
+              
+          }
+
+    })
+    .catch(function(error) {
+      console.error("Error getting: ", error);
+  });
+
+    return data;
+
+}
+
+// getSpecieNameFromSPid(4)
+
+async function IncrementSpecie(Specie_ID){
+  let count;
+  let snapshot = await db.collection("species").where("Specie_ID",'==',Specie_ID).get()
+    .then(snap => {
+      // console.log(snap)
+      if (snap.empty) {
+              console.log('query failed')
+              // console.log("len ", Object.keys(data).length);
+
+              return "empty"
+              // return data;
+              
+          }
+          else
+          {
+              // console.log("query completed in:" , new Date() - start)
+              const data_fetched = snap.docs.map(doc => doc.data());
+              data = data_fetched[0]
+               count= data ['Occurences']
+
+              // return {data}
+              
+          }
+
+    })
+    .catch(function(error) {
+      console.error("Error getting: ", error);
+  });
+
+  //   await database.collection("species").where("Specie_ID",'==',Specie_ID).update({
+  //     Occurences: count + 1
+  //   })
+
+  db.collection("species").where("Specie_ID", "==", Specie_ID)
+  .get()
+  .then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+          console.log(doc.id, " => ", doc.data());
+                    
+          // Build doc ref from doc.id
+          db.collection("species").doc(doc.id).update({Occurences: count +1 });
+      });
+ })
+
+}
+
+
+async function IncrementPlantOnPerson(User_ID){
+  let count;
+  let snapshot = await db.collection("users").where("User_ID",'==',User_ID).get()
+    .then(snap => {
+      // console.log(snap)
+      if (snap.empty) {
+              console.log('query failed IncrementPlantOnPerson')
+              // console.log("len ", Object.keys(data).length);
+
+              return "empty"
+              // return data;
+              
+          }
+          else
+          {
+              // console.log("query completed in:" , new Date() - start)
+              const data_fetched = snap.docs.map(doc => doc.data());
+              data = data_fetched[0]
+               count= data ['Tree']
+
+              // return {data}
+              
+          }
+
+    })
+    .catch(function(error) {
+      console.error("Error getting: ", error);
+  });
+
+  //   await database.collection("species").where("Specie_ID",'==',Specie_ID).update({
+  //     Occurences: count + 1
+  //   })
+
+  db.collection("species").where("User_ID", "==", User_ID)
+  .get()
+  .then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+          console.log(doc.id, " => ", doc.data());
+                    
+          // Build doc ref from doc.id
+          db.collection("users").doc(doc.id).update({Tree: count +1 });
+      });
+ })
+
+}
+
+
+
+async function Add_Tree_Details (tree_info){
+
+  if (tree_info === undefined) {
+        return;
+    }
+  
+  let unique_id
+  flag = true
+  while(flag){
+    
+    unique_id = await getRndInteger(1, 10000)
+    console.log(unique_id)
+    let res = await Confirm_UniqueID("plants", "Plant_ID", unique_id)
+    // console.log(res)
+    if (res == "empty")  {
+      flag = false
+      break
+    }
+  }
+
+  let specie_res = await getSpecieNameFromSPid(tree_info['Specie_ID'])
+
+  console.log()
+
+  var start = new Date();
+  var Date_Now = new Date();
+  // Date_Now.format("mm/dd/yy");
+  
+  let tree_item = {
+      Location: tree_info['Location'],
+      image : tree_info['Picture'],
+      Name : tree_info['Name'],
+      Plant_ID : unique_id,
+      Planter_ID: tree_info['Planter_ID'],
+      Specie_Bioname:specie_res['Specie_bioName'],
+      Specie_ID : tree_info['Specie_ID'],
+      Specie_Name :specie_res['Specie_Name'],
+      Watered: start,
+      Date_Planted: start,
+      
+  };
+
+  // console.log("here")
+
+  let obj = await db.collection('plants').add(tree_item)
+    .then(snap =>
+    {
+      if(snap.empty){
+        console.log("null")
+        return null
+      }
+      else{
+        console.log("issue here")
+        // console.log(snap)
+        return snap
+      }
+    })
+
+
+  await IncrementSpecie(tree_item['Specie_ID'])
+  await IncrementPlantOnPerson(tree_item['Planter_ID'])
+  console.log("done adding Plant details, and settling all the tables")
+  return "done";
+
+}
+
+
+
+////////////////////////////////////////////////////////////////////
+
 
 export {
     signin,
@@ -307,4 +557,10 @@ export {
     getSpecieFunc,
     getSingleSpecie,
     get_AllSpecies,
+    Add_Tree_Details,
+    IncrementSpecie,
+    IncrementPlantOnPerson,
+    getSpecieNameFromSPid,
+    Confirm_UniqueID,
+    getRndInteger,
 };
